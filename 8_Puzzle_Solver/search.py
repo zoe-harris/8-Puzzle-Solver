a26c4d32120007ad4d85aa8e56d95774d01192f2
raelewis <relewis@me.com>
2019-09-09 15:34:24 -0800

    -Added skeleton comments to describe variables & methods -Began def __init__

--- 8_Puzzle_Solver/search.py
+++ 8_Puzzle_Solver/search.py
@@ -4,9 +4,23 @@
 
 import node.py
 
-#VARIABLES:
-    #search_start
-    #search_goal
-    #closed_list
-    #open_list
-    #search_type
+class search:
+    def __init__(self, start, goal, closed, open, search):
+        test = 0
+
+
+""" VARIABLES:
+    search_start  # This holds the starting state of the puzzle
+    search_goal  # This holds the solution to the puzzle
+    closed_list  # This is a list that holds the nodes that have been expanded
+    open_list  # This is a list that holds the nodes that need to be expanded
+    search_type  # This denotes which search method will be used to solve the puzzle
+     
+     METHODS
+     check_parody  # This method will determine the parody of the puzzle to determine is it is solveable
+     generate_options  # This method creates a node for each viable move & adds the node to the open_list
+     check_duplicate  # This method checks to see if the current node is located in the closed list. If it is, it is
+      removed from the open_list.
+     check_solution  # This method compares the current node to search_goal.
+     breadth_first_search  # This method runs the BFS puzzle solver.
+     """
