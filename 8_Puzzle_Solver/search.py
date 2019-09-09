a720803d44d90b113e434217f1bb97c491679b53
raelewis <relewis@me.com>
2019-09-09 15:51:19 -0800

    Finished the skeleton of search.py

--- 8_Puzzle_Solver/search.py
+++ 8_Puzzle_Solver/search.py
@@ -12,15 +12,36 @@ class Search:
         self.closed_list = closed  # This is a list that holds the nodes that have been expanded
         self.open_list = o_list  # This is a list that holds the nodes that need to be expanded
 
+    # This method will determine the parody of the puzzle to determine is it is solvable
+    def check_parody(self):
+        print("Hey, some parodies are better than the original. Some are too close. Personal opinion, really.")
 
-""" VARIABLES:
+    # This method creates a node for each viable move & adds the node to the open_list
+    def generate_options(self):
+        print("I mean we could get coffee, or tea, or a smoothie.")
 
-     
-     METHODS
-     check_parody  # This method will determine the parody of the puzzle to determine is it is solveable
-     generate_options  # This method creates a node for each viable move & adds the node to the open_list
-     check_duplicate  # This method checks to see if the current node is located in the closed list. If it is, it is
-      removed from the open_list.
-     check_solution  # This method compares the current node to search_goal.
-     breadth_first_search  # This method runs the BFS puzzle solver.
-     """
+    # This method checks to see if the current node is located in the closed list. If it is, it is removed from the
+    # open_list.
+    def check_duplicate(self):
+        print("Duplicates are boring. Be original.")
+
+    # This method compares the current node to search_goal.
+    def check_solution(self):
+        print("Testing 1,2,3.")
+
+    # This method runs the BFS puzzle solver.
+    def breadth_first_search(self):
+        print("Congrats, you've made it to BFS! If only it were a BLT.")
+
+    # This method runs the misplaced tiles puzzle solver.
+    def misplaced_tiles(self):
+        print("Misplaced tiles!? How are we supposed to finish the flooring now?!")
+
+    # This method runs the manhattan distance puzzle solver
+    def manhattan_distance(self):
+        print("Welcome to manhattan distance. The distance to Manhattan 4,376.1 miles. Wait, this isn't what"
+              " we're supposed to do? Lame.")
+
+    # This method runs the gaschnig puzzle solver.
+    def gaschnig(self):
+        print("Welcome to gaschnig. I'm not sure what this is but it sounds cool.")
