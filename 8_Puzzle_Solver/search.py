cd369970de629de30808108edd029ed001dd60e7
Your Name <you@example.com>
2019-09-09 16:22:16 -0800

    Completed generate_options method in search.py

--- 8_Puzzle_Solver/search.py
+++ 8_Puzzle_Solver/search.py
@@ -2,10 +2,11 @@
 # CSCE405 Artificial Intelligence
 # Programming Assignment #1
 
-# import node.py
+from node import Node
+
 
 class Search:
-    def __init__(self, search, start, goal, closed, o_list):
+    def __init__(self, search=None, start=None, goal=None, closed=None, o_list=None):
         self.search_type = search  # This string denotes which search method will be used to solve the puzzle
         self.search_start = start  # This list the starting state of the puzzle
         self.search_goal = goal  # This list the solution to the puzzle
@@ -17,8 +18,37 @@ class Search:
         print("Hey, some parodies are better than the original. Some are too close. Personal opinion, really.")
 
     # This method creates a node for each viable move & adds the node to the open_list
-    def generate_options(self):
-        print("I mean we could get coffee, or tea, or a smoothie.")
+    def generate_options(self, curr_node):
+
+        blank_pos = curr_node.val.index('X')
+        options = []
+
+        # UP
+        if blank_pos > 2:
+            new_list = curr_node.val.copy()
+            new_list[blank_pos], new_list[blank_pos - 3] = new_list[blank_pos - 3], new_list[blank_pos]
+            n = Node(new_list, curr_node, curr_node.g + 1, None, None)
+            options.append(n)
+        # DOWN
+        if blank_pos < 6:
+            new_list = curr_node.val.copy()
+            new_list[blank_pos], new_list[blank_pos + 3] = new_list[blank_pos + 3], new_list[blank_pos]
+            n = Node(new_list, curr_node, curr_node.g + 1, None, None)
+            options.append(n)
+        # LEFT
+        if blank_pos is not 0 and blank_pos is not 3 and blank_pos is not 6:
+            new_list = curr_node.val.copy()
+            new_list[blank_pos], new_list[blank_pos - 1] = new_list[blank_pos - 1], new_list[blank_pos]
+            n = Node(new_list, curr_node, curr_node.g + 1, None, None)
+            options.append(n)
+        # RIGHT
+        if blank_pos is not 2 and blank_pos is not 5 and blank_pos is not 8:
+            new_list = curr_node.val.copy()
+            new_list[blank_pos], new_list[blank_pos + 1] = new_list[blank_pos + 1], new_list[blank_pos]
+            n = Node(new_list, curr_node, curr_node.g + 1, None, None)
+            options.append(n)
+
+        return options
 
     # This method checks to see if the current node is located in the closed list. If it is, it is removed from the
     # open_list.
