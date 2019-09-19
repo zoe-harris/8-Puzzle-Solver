c5bf20935e12b2a058386a3ca69fde37e7bd512e
raelewis <relewis@me.com>
2019-09-18 16:13:22 -0800

    Fixed a bug where an unequal parity puzzle could be run through the program.

--- 8_Puzzle_Solver/main.py
+++ 8_Puzzle_Solver/main.py
@@ -27,24 +27,30 @@ def main():
         g = goal_string.split()
 
         # Check parities or start and goal states
-        if not search_obj.equal_parity(s, g):
-            print("The state you entered did not have the same parity. This puzzle is unsolvable.")
+        while not search_obj.equal_parity(s, g):
+            print("The state you entered did not have the same parity. This puzzle is unsolvable. "
+                  "\nPlease input a new puzzle")
+            start_string = input("Enter start state (separate tiles with a space and mark blank tile with an X): ")
+            s = start_string.split()
+
+            goal_string = input("Enter goal state (separate tiles with a space and mark blank tile with an X): ")
+            g = goal_string.split()
 
         convert_int(s)
         convert_int(g)
 
         # Store states as 2D lists
         start_state = [
-                       [s[0], s[1], s[2]],
-                       [s[3], s[4], s[5]],
-                       [s[6], s[7], s[8]],
-                      ]
+            [s[0], s[1], s[2]],
+            [s[3], s[4], s[5]],
+            [s[6], s[7], s[8]],
+        ]
 
         goal_state = [
-                       [g[0], g[1], g[2]],
-                       [g[3], g[4], g[5]],
-                       [g[6], g[7], g[8]],
-                     ]
+            [g[0], g[1], g[2]],
+            [g[3], g[4], g[5]],
+            [g[6], g[7], g[8]],
+        ]
 
         # make Search object
         # search = Search(Node(start_state), Node(goal_state))
@@ -85,5 +91,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
